a006ce1e3efd7291a84d03aefa4bf8d5a3eb04d3
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-01-11 17:24:52 +0100

    created dcoekr compose file with 2 services server and psql db

--- kingsleague/build.gradle.kts
+++ kingsleague/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("jacoco")
 	kotlin("jvm") version "1.9.21"
 	kotlin("plugin.spring") version "1.9.21"
 	kotlin("plugin.jpa") version "1.9.21"
