5fdaeb6dd09184266acce34a764e9bb1b32fd7b3
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-01-11 15:08:59 +0100

    created the backend project

---
+++ kingsleague/src/main/kotlin/com/youcode/kingsleague/KingsleagueApplication.kt
@@ -0,0 +1,11 @@
+package com.youcode.kingsleague
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KingsleagueApplication
+
+fun main(args: Array<String>) {
+	runApplication<KingsleagueApplication>(*args)
+}
---
+++ kingsleague/src/test/kotlin/com/youcode/kingsleague/KingsleagueApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.youcode.kingsleague
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KingsleagueApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
