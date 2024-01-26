0848d5af9daa23024ae8fac5bce9df33a40d8fe1
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-01-26 11:48:00 +0100

    completed the 3 main services eureka and config and gateway services

--- organizer_service/src/main/kotlin/com/youcode/kingsleague/organizer_service/OrganizerServiceApplication.kt
+++ organizer_service/src/main/kotlin/com/youcode/kingsleague/organizer_service/OrganizerServiceApplication.kt
@@ -3,9 +3,11 @@ package com.youcode.kingsleague.organizer_service
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.context.config.annotation.RefreshScope
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RefreshScope
 class OrganizerServiceApplication
 
 fun main(args: Array<String>) {
--- tournament_service/src/main/kotlin/com/youcode/kingsleague/tournament_service/TournamentServiceApplication.kt
+++ tournament_service/src/main/kotlin/com/youcode/kingsleague/tournament_service/TournamentServiceApplication.kt
@@ -3,9 +3,11 @@ package com.youcode.kingsleague.tournament_service
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.context.config.annotation.RefreshScope
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RefreshScope
 class TournamentServiceApplication
 
 fun main(args: Array<String>) {
