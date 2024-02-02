41a18248892e820c99e9256568a76473cd6ebe37
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-02-02 10:30:04 +0100

    added creation and update timestamp for tournament entities

--- tournament_service/src/main/kotlin/com/youcode/kingsleague/tournament_service/models/entities/League.kt
+++ tournament_service/src/main/kotlin/com/youcode/kingsleague/tournament_service/models/entities/League.kt
@@ -5,8 +5,19 @@ import jakarta.persistence.Entity
 import jakarta.persistence.Table
 import lombok.Builder
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "leagues")
 @Builder
-class League (id: Long, name: String, debutDate: LocalDate, endDate: LocalDate, location: String, organizerId: Long, organizer: Organizer): Tournament(id, name, debutDate, endDate, location, organizerId, organizer)
+class League (
+    id: Long,
+    name: String,
+    debutDate: LocalDate,
+    endDate: LocalDate,
+    location: String,
+    organizerId: Long,
+    organizer: Organizer,
+    createdAt: LocalDateTime,
+    updatedAt: LocalDateTime
+) : Tournament(id, name, debutDate, endDate, location, organizerId, organizer, createdAt, updatedAt)
