46554067d24dcf16ac06204c0d0f78014b3ddc59
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-02-06 19:38:38 +0100

    completed the methods in teamService

--- team_service/src/main/kotlin/com/youcode/kingsleague/team_service/services/TeamService.kt
+++ team_service/src/main/kotlin/com/youcode/kingsleague/team_service/services/TeamService.kt
@@ -1,12 +1,7 @@
 package com.youcode.kingsleague.team_service.services
 
+import com.youcode.kingsleague.common.services.GlobalService
 import com.youcode.kingsleague.team_service.models.dto.TeamDTO
 
-interface TeamService {
-
-    fun save(team: TeamDTO): TeamDTO
-    fun findAll(): List<TeamDTO>
-    fun findByID(id: Long): TeamDTO
-    fun update(id: Long, team: TeamDTO): TeamDTO
-    fun delete(id: Long)
+interface TeamService: GlobalService<TeamDTO, Long> {
 }
--- team_service/src/main/kotlin/com/youcode/kingsleague/team_service/services/impl/TeamServiceImpl.kt
+++ team_service/src/main/kotlin/com/youcode/kingsleague/team_service/services/impl/TeamServiceImpl.kt
@@ -1,34 +1,52 @@
 package com.youcode.kingsleague.team_service.services.impl
 
-import com.youcode.kingsleague.common.config.MapperConfig
+import com.youcode.kingsleague.common.exceptions.ResourceNotFoundException
 import com.youcode.kingsleague.team_service.models.dto.TeamDTO
 import com.youcode.kingsleague.team_service.models.entities.Team
 import com.youcode.kingsleague.team_service.repositories.TeamRepository
 import com.youcode.kingsleague.team_service.services.TeamService
+import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
 
 @Service
-class TeamServiceImpl(private val teamRepository: TeamRepository, private val modelMapper: MapperConfig): TeamService {
-    override fun save(team: TeamDTO): TeamDTO {
-        val teamEntity: Team = modelMapper.map(team, Team::class.java)
+class TeamServiceImpl(private val teamRepository: TeamRepository, private val modelMapper: ModelMapper): TeamService {
+    override fun save(dto: TeamDTO?): TeamDTO? {
+        val teamEntity: Team = modelMapper.map(dto, Team::class.java)
         val savedTeam: Team = teamRepository.save(teamEntity)
         return modelMapper.map(savedTeam, TeamDTO::class.java)
     }
 
-    override fun findAll(): List<TeamDTO> {
-        TODO("Not yet implemented")
+    override fun getAll(): List<TeamDTO?>? {
+        val teams: List<Team> = teamRepository.findAll()
+        return teams.map { team -> modelMapper.map(team, TeamDTO::class.java) }
     }
 
-    override fun findByID(id: Long): TeamDTO {
-        return teamRepository.findById(id).
-        TODO("Not yet implemented")
+    override fun update(identifier: Long, dto: TeamDTO?): TeamDTO? {
+        val existingTeam: Team = teamRepository.findById(identifier)
+            .orElseThrow { ResourceNotFoundException("Team with id $identifier not found") }
+
+        existingTeam.apply {
+            dto?.let {
+                this.name = it.name
+                this.country = it.country
+            }
+        }
+
+        val updatedTeam: Team = teamRepository.save(existingTeam)
+        return modelMapper.map(updatedTeam, TeamDTO::class.java)
     }
 
-    override fun update(id: Long, team: TeamDTO): TeamDTO {
-        TODO("Not yet implemented")
+    override fun delete(identifier: Long) {
+        if (!teamRepository.existsById(identifier))
+            throw ResourceNotFoundException("Team with id $identifier not found")
+        teamRepository.deleteById(identifier)
     }
 
-    override fun delete(id: Long) {
-        TODO("Not yet implemented")
+    override fun findByID(identifier: Long): TeamDTO? {
+        val team: Team = teamRepository.findById(identifier).orElseThrow {
+            ResourceNotFoundException("Team with id $identifier not found")
+        }
+        return modelMapper.map(team, TeamDTO::class.java)
     }
+
 }
