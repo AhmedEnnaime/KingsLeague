cd5a5382f446840e57f4f9a5d402d01baa79038f
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-02-14 11:50:51 +0100

    implemented cup service

--- tournament-service/src/main/kotlin/com/youcode/kingsleague/tournamentservice/services/impl/CupServiceImpl.kt
+++ tournament-service/src/main/kotlin/com/youcode/kingsleague/tournamentservice/services/impl/CupServiceImpl.kt
@@ -1,30 +1,56 @@
 package com.youcode.kingsleague.tournamentservice.services.impl
 
+import com.youcode.kingsleague.common.exceptions.ResourceNotFoundException
 import com.youcode.kingsleague.tournamentservice.models.dto.CupDTO
+import com.youcode.kingsleague.tournamentservice.models.entities.Cup
 import com.youcode.kingsleague.tournamentservice.repositories.CupRepository
 import com.youcode.kingsleague.tournamentservice.services.CupService
 import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class CupServiceImpl(private val cupRepository: CupRepository, private val modelMapper: ModelMapper): CupService {
     override fun save(dto: CupDTO): CupDTO {
-        TODO("Not yet implemented")
+        dto.createdAt = LocalDateTime.now()
+        dto.updatedAt = LocalDateTime.now()
+        val cupEntity: Cup = modelMapper.map(dto, Cup::class.java)
+        val savedCup: Cup = cupRepository.save(cupEntity)
+        return modelMapper.map(savedCup, CupDTO::class.java)
     }
 
     override fun getAll(): List<CupDTO?>? {
-        TODO("Not yet implemented")
+        val cups: List<Cup> = cupRepository.findAll()
+        return cups.map { cup -> modelMapper.map(cup, CupDTO::class.java) }
     }
 
     override fun update(identifier: Long, dto: CupDTO): CupDTO {
-        TODO("Not yet implemented")
+        val existingCup: Cup = cupRepository.findById(identifier).orElseThrow{
+            ResourceNotFoundException("Cup with $identifier not found")
+        }
+        dto.updatedAt = LocalDateTime.now()
+        existingCup.apply {
+            dto.let {
+                this.name = it.name
+                this.location = it.location
+                this.debutDate = it.debutDate
+                this.endDate = it.endDate
+            }
+        }
+        val updatedCup: Cup = cupRepository.save(existingCup)
+        return modelMapper.map(updatedCup, CupDTO::class.java)
     }
 
     override fun delete(identifier: Long) {
-        TODO("Not yet implemented")
+        if (!cupRepository.existsById(identifier))
+            throw ResourceNotFoundException("Cup with id $identifier not found")
+        cupRepository.deleteById(identifier)
     }
 
     override fun findByID(identifier: Long): CupDTO? {
-        TODO("Not yet implemented")
+        val cup: Cup = cupRepository.findById(identifier).orElseThrow {
+            ResourceNotFoundException("Cup with $identifier not found")
+        }
+        return modelMapper.map(cup, CupDTO::class.java)
     }
 }
