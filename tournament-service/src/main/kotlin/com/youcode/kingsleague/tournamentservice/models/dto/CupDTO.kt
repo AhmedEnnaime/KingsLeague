60846216ba51826d567a586ab98a32d28299e185
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-02-14 14:47:22 +0100

    fixed a small bug in cup dto

--- tournament-service/src/main/kotlin/com/youcode/kingsleague/tournamentservice/models/dto/CupDTO.kt
+++ tournament-service/src/main/kotlin/com/youcode/kingsleague/tournamentservice/models/dto/CupDTO.kt
@@ -1,19 +1,21 @@
 package com.youcode.kingsleague.tournamentservice.models.dto
 
-import com.youcode.kingsleague.tournamentservice.models.dto.TournamentDTO
 import com.youcode.kingsleague.tournamentservice.models.transients.Organizer
-import lombok.Builder
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-@Builder
-class CupDTO(
-    id: Long,
-    name: String,
-    debutDate: LocalDate,
-    endDate: LocalDate,
-    location: String,
-    organizer: Organizer?,
-    createdAt: LocalDateTime?,
-    updatedAt: LocalDateTime?,
-) : TournamentDTO(id, name, debutDate, endDate, location, organizer, createdAt, updatedAt)
+class CupDTO : TournamentDTO {
+
+    constructor() : super(null, "", LocalDate.now(), LocalDate.now(), "", null, null, null)
+
+    constructor(
+        id: Long?,
+        name: String,
+        debutDate: LocalDate,
+        endDate: LocalDate,
+        location: String,
+        organizer: Organizer?,
+        createdAt: LocalDateTime?,
+        updatedAt: LocalDateTime?
+    ) : super(id, name, debutDate, endDate, location, organizer, createdAt, updatedAt)
+}
