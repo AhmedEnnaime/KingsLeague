1cae4cd747ece8498690c3f17d0340e653d726d4
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-02-14 15:08:53 +0100

    implemented the cup controller

--- tournament-service/src/main/kotlin/com/youcode/kingsleague/tournamentservice/controllers/CupController.kt
+++ tournament-service/src/main/kotlin/com/youcode/kingsleague/tournamentservice/controllers/CupController.kt
@@ -1,4 +1,15 @@
 package com.youcode.kingsleague.tournamentservice.controllers
 
-class CupController {
+import GlobalController
+import com.youcode.kingsleague.tournamentservice.models.dto.CupDTO
+import com.youcode.kingsleague.tournamentservice.services.CupService
+import org.springframework.http.MediaType
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1/cups", produces = [MediaType.APPLICATION_JSON_VALUE])
+@Validated
+class CupController(private val cupService: CupService): GlobalController<CupDTO, Long>() {
 }
