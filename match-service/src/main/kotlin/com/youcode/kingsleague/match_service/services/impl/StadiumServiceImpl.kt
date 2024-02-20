11a0e591aba36d761d391dc5807e5df6afef8eb1
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-02-20 12:13:27 +0100

    working on implementing methods in stadium service

--- match-service/src/main/kotlin/com/youcode/kingsleague/match_service/services/impl/StadiumServiceImpl.kt
+++ match-service/src/main/kotlin/com/youcode/kingsleague/match_service/services/impl/StadiumServiceImpl.kt
@@ -1,19 +1,27 @@
 package com.youcode.kingsleague.match_service.services.impl
 
+import com.youcode.kingsleague.common.exceptions.ResourceNotFoundException
 import com.youcode.kingsleague.match_service.models.dto.StadiumDTO
+import com.youcode.kingsleague.match_service.models.entities.Stadium
 import com.youcode.kingsleague.match_service.repositories.StadiumRepository
 import com.youcode.kingsleague.match_service.services.StadiumService
 import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class StadiumServiceImpl(private val modelMapper: ModelMapper, private val stadiumRepository: StadiumRepository): StadiumService {
     override fun save(dto: StadiumDTO): StadiumDTO {
-        TODO("Not yet implemented")
+        dto.createdAt = LocalDateTime.now()
+        dto.updatedAt = LocalDateTime.now()
+        val stadiumEntity: Stadium = modelMapper.map(dto, Stadium::class.java)
+        val savedStadium: Stadium = stadiumRepository.save(stadiumEntity)
+        return modelMapper.map(savedStadium, StadiumDTO::class.java)
     }
 
     override fun getAll(): List<StadiumDTO?>? {
-        TODO("Not yet implemented")
+        val stadiums: List<Stadium> = stadiumRepository.findAll()
+        return stadiums.map { stadium -> modelMapper.map(stadium, StadiumDTO::class.java) }
     }
 
     override fun update(identifier: Long, dto: StadiumDTO): StadiumDTO {
@@ -21,10 +29,15 @@ class StadiumServiceImpl(private val modelMapper: ModelMapper, private val stadi
     }
 
     override fun delete(identifier: Long) {
-        TODO("Not yet implemented")
+        if (!stadiumRepository.existsById(identifier))
+            throw ResourceNotFoundException("Stadium with id $identifier not found")
+        stadiumRepository.deleteById(identifier)
     }
 
     override fun findByID(identifier: Long): StadiumDTO? {
-        TODO("Not yet implemented")
+        val stadium: Stadium = stadiumRepository.findById(identifier).orElseThrow {
+            ResourceNotFoundException("Stadium with id $identifier not found")
+        }
+        return modelMapper.map(stadium, StadiumDTO::class.java)
     }
 }
