1a9a31a8f978373f029fce0f28590f427cacab44
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-02-28 19:22:51 +0100

    started working on tournament teams microservice

--- tournament-service/src/main/kotlin/com/youcode/kingsleague/tournamentservice/TournamentServiceApplication.kt
+++ tournament-service/src/main/kotlin/com/youcode/kingsleague/tournamentservice/TournamentServiceApplication.kt
@@ -4,12 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-//@ComponentScan("com.youcode.kingsleague.common.config")
 class TournamentServiceApplication
 
 fun main(args: Array<String>) {
--- tournamentTeams-service/src/main/kotlin/com/youcode/kingsleague/tournamentteamsservice/models/dto/TournamentTeamDTO.kt
+++ tournamentTeams-service/src/main/kotlin/com/youcode/kingsleague/tournamentteamsservice/models/dto/TournamentTeamDTO.kt
@@ -3,9 +3,7 @@ package com.youcode.kingsleague.tournamentteamsservice.models.dto
 import com.youcode.kingsleague.tournamentteamsservice.models.embeddables.TournamentTeamKey
 import com.youcode.kingsleague.tournamentteamsservice.models.transients.Team
 import com.youcode.kingsleague.tournamentteamsservice.models.transients.Tournament
-import lombok.Builder
 
-@Builder
 data class TournamentTeamDTO (
     var id: TournamentTeamKey?,
     var team: Team?,
