8c50b9d17291da12d59cf3600fe8340f7d9b9366
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-02-28 12:27:48 +0100

    created 2 endpoints for 2 methods

--- match-service/src/main/kotlin/com/youcode/kingsleague/match_service/controllers/MatchRefereeController.kt
+++ match-service/src/main/kotlin/com/youcode/kingsleague/match_service/controllers/MatchRefereeController.kt
@@ -1,12 +1,16 @@
 package com.youcode.kingsleague.match_service.controllers
 
+import com.youcode.kingsleague.match_service.models.dto.MatchDTO
 import com.youcode.kingsleague.match_service.models.dto.MatchRefereeDTO
+import com.youcode.kingsleague.match_service.models.dto.RefereeDTO
 import com.youcode.kingsleague.match_service.services.MatchRefereeService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,5 +25,17 @@ class MatchRefereeController(private val matchRefereeService: MatchRefereeServic
     fun assignRefereeToMatch(@RequestBody @Valid matchRefereeDTO: MatchRefereeDTO): ResponseEntity<MatchRefereeDTO> {
         val savedMatchReferee: MatchRefereeDTO = matchRefereeService.assignRefereeToMatch(matchRefereeDTO)
         return ResponseEntity(savedMatchReferee, HttpStatus.CREATED)
+    }
+
+    @GetMapping("/referees/{matchId}")
+    fun findMatchReferees(@PathVariable matchId: Long): ResponseEntity<List<RefereeDTO>> {
+        val referees: List<RefereeDTO> = matchRefereeService.findMatchReferees(matchId)
+        return ResponseEntity(referees, HttpStatus.OK)
+    }
+
+    @GetMapping("/matches/{refereeId}")
+    fun findRefereeMatches(@PathVariable refereeId: Long): ResponseEntity<List<MatchDTO>> {
+        val matches: List<MatchDTO> = matchRefereeService.findRefereeMatches(refereeId)
+        return ResponseEntity(matches, HttpStatus.OK)
     }
 }
