e072b9803c9c7e74ad6ccf4394dc787eab163318
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-03-04 19:23:11 +0100

    fixed the error in gateway service and still working on config server service to fix a bug in fetching resources from github

--- gateway_service/src/main/kotlin/com/youcode/kingsleague/gateway_service/GatewayServiceApplication.kt
+++ gateway_service/src/main/kotlin/com/youcode/kingsleague/gateway_service/GatewayServiceApplication.kt
@@ -2,12 +2,14 @@ package com.youcode.kingsleague.gateway_service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class GatewayServiceApplication {
 	@Bean
 	fun dynamicRoutes(rdc: ReactiveDiscoveryClient, dlp: DiscoveryLocatorProperties): DiscoveryClientRouteDefinitionLocator {
