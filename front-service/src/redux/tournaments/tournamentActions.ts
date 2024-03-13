f6beccbc4f998ec0409499301a629d30bf02537e
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-03-13 09:54:45 +0000

    created crud actions and slice for tournament

--- front-service/src/redux/tournaments/tournamentActions.ts
+++ front-service/src/redux/tournaments/tournamentActions.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import ITournament from "../../interfaces/ITournament";
 import API from "../../utils/API";
+import { AxiosResponse } from "axios";
 
 export const fetchAllTournaments = createAsyncThunk<ITournament[]>(
   "tournament/all",
@@ -9,3 +10,35 @@ export const fetchAllTournaments = createAsyncThunk<ITournament[]>(
     return data;
   }
 );
+
+export const createTournament = createAsyncThunk<
+  ITournament,
+  Partial<ITournament>
+>("tournament/create", async (tournament) => {
+  const { data } = await API.post(
+    `/TOURNAMENT-SERVICE/api/v1/tournaments`,
+    tournament
+  );
+  return data;
+});
+
+export const deleteTournament = createAsyncThunk<Map<string, string>, number>(
+  "tournament/delete",
+  async (id) => {
+    const response: AxiosResponse = await API.delete(
+      `/TOURNAMENT-SERVICE/api/v1/tournaments/${id}`
+    );
+    return response.data;
+  }
+);
+
+export const updateTournament = createAsyncThunk<
+  ITournament,
+  { id: number; tournamentData: Partial<ITournament> }
+>("tournament/update", async ({ id, tournamentData }) => {
+  const { data } = await API.put(
+    `/TOURNAMENT-SERVICE/api/v1/tournaments/${id}`,
+    tournamentData
+  );
+  return data;
+});
--- front-service/src/redux/tournaments/tournamentSlice.ts
+++ front-service/src/redux/tournaments/tournamentSlice.ts
@@ -1,6 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import ITournament from "../../interfaces/ITournament";
-import { fetchAllTournaments } from "./tournamentActions";
+import {
+  createTournament,
+  deleteTournament,
+  fetchAllTournaments,
+  updateTournament,
+} from "./tournamentActions";
 
 interface TournamentState {
   tournaments: ITournament[];
@@ -23,6 +28,33 @@ const tournamentSlice = createSlice({
       .addCase(fetchAllTournaments.fulfilled, (state, action) => {
         state.loading = false;
         state.tournaments = action.payload;
+      })
+      .addCase(createTournament.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createTournament.fulfilled, (state, action) => {
+        state.loading = false;
+        state.tournaments.push(action.payload);
+      })
+      .addCase(deleteTournament.pending, (state, action) => {
+        state.loading = true;
+        state.tournaments = state.tournaments.filter(
+          (tournament) => tournament.id !== action.meta.arg
+        );
+      })
+      .addCase(updateTournament.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(updateTournament.fulfilled, (state, action) => {
+        state.loading = false;
+        const updatedTournamentId = action.meta.arg.id;
+        const updatedTournament = action.payload;
+        const existingTournament = state.tournaments.find(
+          (tournament) => tournament.id === updatedTournamentId
+        );
+        if (existingTournament) {
+          Object.assign(existingTournament, updatedTournament);
+        }
       });
   },
   reducers: {},
